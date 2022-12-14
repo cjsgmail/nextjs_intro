751b661ec0f193bfcaa6c2e8e847b94a00bc0697
cjsgmail <cjs0097@gmail.com>
2022-12-14 20:37:23 +0900

    feat: contact me

--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,11 @@ import Header from "../components/Header";
 import styled from "styled-components";
 import ProfileImg from "../images/ProfileImg.webp";
 import Image from "next/image";
+import Link from "next/link";
 
 const MainCotainer = styled.main`
   width: 100%;
+  height: 100%;
   display: flex;
   flex-direction: column;
   position: absolute;
@@ -33,6 +35,38 @@ const H1 = styled.div`
   margin-left: 30px;
 `;
 
+const ContactContainer = styled.div`
+  height: 700px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  color: #3b5bdb;
+`;
+
+const AboutContainer = styled.div`
+  height: 300px;
+  padding: 20px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: black;
+`;
+
+const AboutContent = styled.div`
+  border-right: 3px solid #edf2ff;
+  padding: 0 20px;
+`;
+
+const GitLink = styled.a`
+  color: black;
+`;
+
+const H2 = styled.h2`
+  color: #3b5bdb;
+  margin-bottom: 20px;
+`;
+
 export default function Home() {
   return (
     <div>
@@ -53,6 +87,29 @@ export default function Home() {
             최정석의 포트폴리오 입니다.
           </H1>
         </MyIntroContainer>
+        <ContactContainer>
+          <H1>About Me</H1>
+          <AboutContainer>
+            <AboutContent>
+              <H2>생년월일</H2>
+              <div>1997.06.02</div>
+            </AboutContent>
+            <AboutContent>
+              <H2>전화번호</H2>
+              <div>010-2035-9863</div>
+            </AboutContent>
+            <AboutContent>
+              <H2>이메일</H2>
+              <div>cjs0097@gmail.com</div>
+            </AboutContent>
+            <AboutContent>
+              <H2>깃허브</H2>
+              <GitLink href="https://github.com/cjsgmail">
+                https://github.com/cjsgmail
+              </GitLink>
+            </AboutContent>
+          </AboutContainer>
+        </ContactContainer>
       </MainCotainer>
     </div>
   );
