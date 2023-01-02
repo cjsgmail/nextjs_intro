d1981d28712a96a51463e49345970825da548d29
cjsgmail <cjs0097@gmail.com>
2023-01-02 17:33:05 +0900

    feat: project component & learn notion API

--- pages/projects/index.js
+++ pages/projects/index.js
@@ -1,10 +1,23 @@
 import Header from "../../components/Header";
+import styled from "styled-components";
+import Project from "../../components/Project";
+
+const ProjectsContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  position: absolute;
+  top: 5rem;
+`;
 
 function PortfolioPage() {
   return (
     <div>
       <Header />
-      <h1>The Portfolio Page</h1>
+      <ProjectsContainer>
+        <Project />
+      </ProjectsContainer>
     </div>
   );
 }
